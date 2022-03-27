74a90c80dc5ef1b969e3f1d1f7817bd2c29876f6
Luke <lukestumfall@gmail.com>
2022-03-26 19:36:09 -1000

    added varibles to js

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number')
+const operationButtons = document.querySelectorAll('[data-operation')
+const equalsButtons = document.querySelector('[data-equals')
+const deleteButtons = document.querySelector('[data-delete')
+const allClearButtons = document.querySelector('[data-all-clear')
+const prevoousOperandTextElement = document.querySelector('[data-previous-operand')
+const CurrentOperandTextElement  = document.querySelector('[data-current-operand')
