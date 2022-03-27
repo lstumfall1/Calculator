60bd7dd8abe825d2d540bc29fd6565ac527cd196
Luke <lukestumfall@gmail.com>
2022-03-26 20:19:38 -1000

    getDisplayNumber split '.'

--- script.js
+++ script.js
@@ -57,7 +57,21 @@ class Calculator {
     }
 
     getDisplayNumber(number) {
-        return number
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits: 0})
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
     }
 
     updateDisplay() {
